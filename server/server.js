021e0dd1891b820c5c732398b327379f60ad5e23
suryaansh2002 <suryaansh2002@gmail.com>
2025-02-03 11:11:55 +0800

    Update

--- server/server.js
+++ server/server.js
@@ -10,16 +10,16 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-// mongoose.connect(process.env.MONGODB_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// });
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
-// const db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-// db.once('open', () => {
-//     console.log('Connected to MongoDB');
-// });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+});
 
 
 app.get('/',(req,res)=>{
